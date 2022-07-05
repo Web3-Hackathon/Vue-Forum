fd22c1c89d83bc3bdc97870a5ac9016b2660b5e5
tinkerbell07 <tinkerbelldeveloper0711@gmail.com>
2022-07-05 10:10:44 +0900

    remove: backend

--- backend/src/index.js
+++
@@ -1,8 +0,0 @@
-const dbConfig = require("../db.config");
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-const db = {};
-db.mongoose = mongoose;
-db.url = dbConfig.url;
-db.users = require("./schema.js")(mongoose);
-module.exports = db;
